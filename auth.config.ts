b58322adc12fbaf8cb509effedeee8cf71b39438
Pedro Baños <pedrobanos.renault@gmail.com>
2024-01-04 13:57:32 +0100

    authenticate done

--- auth.config.ts
+++ auth.config.ts
@@ -1,4 +1,5 @@
 import type { NextAuthConfig } from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
 
 export const authConfig = {
     pages: {
@@ -17,5 +18,10 @@ export const authConfig = {
             return true;
         },
     },
-    providers: [], // Add providers with an empty array for now
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+        }),
+    ], // Add providers with an empty array for now
 } satisfies NextAuthConfig;
